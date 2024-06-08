3c32a6f1ebbb68e06b9654f705d0502194354184
Sediqwe <sediqwe@gmail.com>
2024-06-08 11:53:31 +0200

    Tiny

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -9,13 +9,7 @@ $(document).on('turbo:load', function() {
         top: '0px' // Visszaállítjuk a kezdeti pozíciót
       }, 200);
       
-    });
-    document.getElementById('bekuldes').addEventListener('click', function() {
-      var uzenet = this.dataset.uzenet; // Használjuk a data-* attribútum nevét, pl. data-uzenet
-      var editAreaContent = document.querySelector('.tox-edit-area').innerHTML;
-      alert(editAreaContent);
-  });
-      
+    });      
   
   
   });
